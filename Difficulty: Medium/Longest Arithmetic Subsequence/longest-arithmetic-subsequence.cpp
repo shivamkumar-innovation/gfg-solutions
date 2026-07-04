206046781eeceb69506f941359d560131bbb646b
Shivam Kumar <shivamyuvraj4537@gmail.com>
2026-07-04 17:56:07 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest Arithmetic Subsequence/longest-arithmetic-subsequence.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<unordered_map<int, int>> d;
+
+    long long f(vector<int>&a, int i, int j,int diff) {
+    
+        long long ans = 0;
+        if(d[j].count(diff)==0){
+            ans=2;
+           
+        }
+        else{
+            ans=1+d[j][diff];
+        }
+
+        return d[i][diff] = ans;
+    }
+
+    int lengthOfLongestAP(vector<int>& a) {
+
+
+        int n = a.size();
+        if (n == 1)
+            return n;
+        d.assign(n, {});
+
+        long long ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i-1; j>=0; j--) {
+
+                int diff = a[i] - a[j];
+                    ans = max(ans, f(a,i, j, diff));
+            }
+        }
+
+        return ans;
+    }
+};
