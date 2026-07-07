b469b1c60a6788c9af5f5480e6f4c2480d279bed
Shivam Kumar <shivamyuvraj4537@gmail.com>
2026-07-07 18:53:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/All Palindromic Partitions/all-palindromic-partitions.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public: 
+vector<string>v;
+    void f(string& a, vector<vector<string>>&d, int i) {
+        if (i == a.size())
+            d.push_back(v);
+
+
+        for (int k = i; k < a.size(); k++) {
+
+            int x = i, y = k;
+            bool b = 1;
+
+            while (x < y) {
+                if (a[x] != a[y]) {
+                    b = 0;
+                    break;
+                }
+                x++;
+                y--;
+            }
+
+            if (b){
+                v.push_back(a.substr(i, k - i + 1));
+                 f(a, d, k + 1);
+                 v.pop_back();
+            }
+        }
+    }
+
+    vector<vector<string>> palinParts(string s) {
+        vector<vector<string>>d;
+         f(s, d, 0);
+         return d;
+    }
+};
