a28a0e0ac2ef2511f5b0f55de1910e3be73cb401
Shivam Kumar <shivamyuvraj4537@gmail.com>
2026-08-01 15:00:31 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
+++ Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
@@ -1,53 +1,47 @@
 class Solution {
 public:
-    bool f(vector<vector<int>> &a, vector<int> &vi, vector<int> &var, int i) {
-        vi[i] = 1;
-        var[i] = 1;
 
-        bool x = 0;
-        int j = 0;
-
-        while (j < a[i].size()) {
-            if (vi[a[i][j]] == 0) {
-                x = f(a, vi, var, a[i][j]);
-            }
-            else if (var[a[i][j]] == 1) {
-                return true;
-            }
-
-            if (x) {
-                return true;
-            }
-
-            j++;
-        }
-
-        var[i] = 0;
-        return false;
-    }
-
-    bool isCyclic(int n, vector<vector<int>> &v) {
+     bool isCyclic(int n, vector<vector<int>> &v) {
         vector<vector<int>> a(n);
-
+        vector<int> b(n, 0);
         for (auto &edge : v) {
             int u = edge[0];
             int w = edge[1];
+            b[w]++;
             a[u].push_back(w);
         }
-
         vector<int> vi(n, 0);
-        vector<int> var(n, 0);
-
-        int i = 0;
-        while (i < n) {
-            if (vi[i] == 0) {
-                if (f(a, vi, var, i)) {
-                    return true;
-                }
+        int i=0;
+        queue<int>q;
+        while(i<n){
+            if(b[i]==0){
+                q.push(i);
             }
             i++;
         }
-
-        return false;
+        int y=0;
+        while(!q.empty()){
+            int x=q.front();
+            q.pop();
+            y++;
+            int j=0;
+            while(j<a[x].size()){
+                if(b[a[x][j]]!=0){
+                   b[a[x][j]]--;
+                   if(b[a[x][j]]==0){
+                       q.push(a[x][j]);
+                   }
+                }
+                j++;
+            }
+        }
+        if(y==n){
+            return 0;
+        }
+        return 1;
     }
 };
+
+
+
+
