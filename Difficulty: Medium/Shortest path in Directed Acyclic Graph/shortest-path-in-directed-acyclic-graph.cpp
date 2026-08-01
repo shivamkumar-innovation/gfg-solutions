69fa805a5da296c3ee6c54a0d5445610d07bf26f
Shivam Kumar <shivamyuvraj4537@gmail.com>
2026-08-01 15:55:04 +0530

    Refactor shortest path algorithm for DAG

--- Difficulty: Medium/Shortest path in Directed Acyclic Graph/shortest-path-in-directed-acyclic-graph.cpp
+++ Difficulty: Medium/Shortest path in Directed Acyclic Graph/shortest-path-in-directed-acyclic-graph.cpp
@@ -1,54 +1,88 @@
 class Solution {
-	public:
-	vector<int> shortestPath(int V, int E, vector<vector<int>> & a) {
-		priority_queue<pair<int, int>, vector<pair<int, int>>,
-		greater<pair<int, int>> > p;
-		
-		vector<vector<pair<int, int>> > v(V);
-		
-		vector<int> vi(V, INT_MAX);
-		
-		int i = 0;
-		while (i < a.size()) {
-			int u = a[i][0];
-			int x = a[i][1];
-			int wt = a[i][2];
-			
-			v[u].push_back({x, wt});
-			i++;
-		}
-		
-		p.push({0, 0});
-		vi[0] = 0;
-		
-		while (!p.empty()) {
-			
-			auto b = p.top();
-			p.pop();
-			
-			if (b.first > vi[b.second])
-				continue;
-			
-			for (auto j : v[b.second]) {
-				
-				int node = j.first;
-				int wt = j.second;
-				
-				int c = b.first + wt;
-				
-				if (c < vi[node]) {
-					vi[node] = c;
-					p.push({c, node});
-				}
-			}
-		}
-		 i = 1;
-		while (i<V) {
-			if (vi[i] == INT_MAX) {
-				vi[i] = -1;
-			}
-			i++;
-		}
-		return vi;
-	}
+public:
+    vector<int> g;
+
+    void f(vector<vector<pair<int,int>>> &v, vector<int> &vis, int i) {
+
+        vis[i] = 1;
+
+        int j = 0;
+        while (j < v[i].size()) {
+
+            if (vis[v[i][j].first] == 0) {
+                f(v, vis, v[i][j].first);
+            }
+
+            j++;
+        }
+
+        g.push_back(i);
+    }
+
+    vector<int> shortestPath(int V, int E, vector<vector<int>> &a) {
+
+        vector<vector<pair<int,int>>> v(V);
+
+        int i = 0;
+        while (i < E) {
+
+            int u = a[i][0];
+            int x = a[i][1];
+            int wt = a[i][2];
+
+            v[u].push_back({x, wt});
+
+            i++;
+        }
+
+        vector<int> vis(V, 0);
+
+        i = 0;
+        while (i < V) {
+            if (vis[i] == 0)
+                f(v, vis, i);
+            i++;
+        }
+
+        reverse(g.begin(), g.end());
+
+        vector<int> vi(V, INT_MAX);
+        vi[0] = 0;
+
+        i = 0;
+        while (i < g.size()) {
+
+            int node = g[i];
+
+            if (vi[node] != INT_MAX) {
+
+                int j = 0;
+
+                while (j < v[node].size()) {
+
+                    int x = v[node][j].first;
+                    int wt = v[node][j].second;
+
+                    if (vi[node] + wt < vi[x]) {
+                        vi[x] = vi[node] + wt;
+                    }
+
+                    j++;
+                }
+            }
+
+            i++;
+        }
+
+        i = 0;
+        while (i < V) {
+
+            if (vi[i] == INT_MAX)
+                vi[i] = -1;
+
+            i++;
+        }
+
+        return vi;
+    }
 };
