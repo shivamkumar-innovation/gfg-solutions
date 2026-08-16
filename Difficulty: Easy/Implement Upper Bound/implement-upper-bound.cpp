694e8daaef047e34dca1371db04d1ab6c6a82c61
Shivam Kumar <shivamyuvraj4537@gmail.com>
2026-08-17 00:08:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Implement Upper Bound/implement-upper-bound.cpp
@@ -0,0 +1,21 @@
+class Solution {
+  public:
+    int upperBound(vector<int>& a, int t) {
+        // code here
+      int i=0;
+      int j=a.size()-1;
+
+      int mid=(i+j)/2;
+      int x=a[j];
+      while(i<=j){
+          mid=(i+j)/2;
+          if(a[mid]<=t){
+              i=mid+1;
+          }
+          else{
+              j=mid-1;
+          }
+      }
+      return i;
+    }
+};
